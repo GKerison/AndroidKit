c6fd21e50b9028d37d762a643255d73c497e8628
GKerison <gaoxiangyangprivate@gmail.com>
2015-08-11 11:24:21 +0800

    Update LogKit

--- logkit/src/main/java/cn/kerison/kit/log/LogKit.java
+++ logkit/src/main/java/cn/kerison/kit/log/LogKit.java
@@ -10,186 +10,186 @@ import java.util.Locale;
  */
 public class LogKit {
 
-	/**
-	 * Log level value {@link Log}
-	 */
-	public static final int LOG_VERBOSE = 2;
-	public static final int LOG_DEBUG = 3;
-	public static final int LOG_INFO = 4;
-	public static final int LOG_WARN = 5;
-	public static final int LOG_ERROR = 6;
-	public static final int LOG_ASSERT = 7;
-	public static final int NO_LOG = Integer.MAX_VALUE;
+    /**
+     * Log level value {@link Log}
+     */
+    public static final int LOG_VERBOSE = 2;
+    public static final int LOG_DEBUG = 3;
+    public static final int LOG_INFO = 4;
+    public static final int LOG_WARN = 5;
+    public static final int LOG_ERROR = 6;
+    public static final int LOG_ASSERT = 7;
+    public static final int NO_LOG = Integer.MAX_VALUE;
 
-	private static int DEFAULT_LEVEL = LOG_VERBOSE;
-	private static String DEFAULT_TAG = null;
+    private static int DEFAULT_LEVEL = LOG_VERBOSE;
+    private static boolean IS_SHOW_DETAIL = true;
+    private static String DEFAULT_TAG = "LogKit";
 
-	private LogKit() {
-	}
+    private LogKit() {
+    }
 
-	public static void setLevel(int level) {
-		DEFAULT_LEVEL = level;
-	}
+    public static void setLevel(int level) {
+        DEFAULT_LEVEL = level;
+    }
 
-	public static void setTag(String defaultTag) {
-		DEFAULT_TAG = defaultTag;
-	}
+    public static void setTag(String defaultTag) {
+        DEFAULT_TAG = defaultTag;
+    }
 
-	public static void v(String content) {
+    public static void disableDetail(){
+        IS_SHOW_DETAIL = false;
+    }
 
-		if (DEFAULT_LEVEL <= LOG_VERBOSE) {
-			LogPrint(LOG_VERBOSE, obtainTag(), content);
-		}
-	}
+    public static void v(String content) {
 
-	public static void v(String format, Object... args) {
+        if (DEFAULT_LEVEL <= LOG_VERBOSE) {
+            LogPrint(LOG_VERBOSE, content);
+        }
+    }
 
-		if (DEFAULT_LEVEL <= LOG_VERBOSE) {
-			LogPrint(LOG_VERBOSE, obtainTag(), String.format(format, args));
-		}
-	}
+    public static void v(String format, Object... args) {
 
-	public static void v(String content, Throwable tr) {
+        if (DEFAULT_LEVEL <= LOG_VERBOSE) {
+            LogPrint(LOG_VERBOSE, String.format(format, args));
+        }
+    }
 
-		if (DEFAULT_LEVEL <= LOG_VERBOSE) {
-			LogPrint(LOG_DEBUG, obtainTag(), content, tr);
-		}
-	}
+    public static void v(String content, Throwable tr) {
+
+        if (DEFAULT_LEVEL <= LOG_VERBOSE) {
+            LogPrint(LOG_DEBUG, content, tr);
+        }
+    }
 
 
-	public static void d(String content) {
+    public static void d(String content) {
 
-		if (DEFAULT_LEVEL <= LOG_DEBUG) {
-			LogPrint(LOG_DEBUG, obtainTag(), content);
-		}
-	}
+        if (DEFAULT_LEVEL <= LOG_DEBUG) {
+            LogPrint(LOG_DEBUG, content);
+        }
+    }
 
-	public static void d(String format, Object... args) {
+    public static void d(String format, Object... args) {
 
-		if (DEFAULT_LEVEL <= LOG_DEBUG) {
-			LogPrint(LOG_DEBUG, obtainTag(), String.format(format, args));
-		}
-	}
+        if (DEFAULT_LEVEL <= LOG_DEBUG) {
+            LogPrint(LOG_DEBUG, String.format(format, args));
+        }
+    }
 
-	public static void d(String content, Throwable tr) {
+    public static void d(String content, Throwable tr) {
 
-		if (DEFAULT_LEVEL <= LOG_DEBUG) {
-			LogPrint(LOG_DEBUG, obtainTag(), content, tr);
-		}
-	}
+        if (DEFAULT_LEVEL <= LOG_DEBUG) {
+            LogPrint(LOG_DEBUG, content, tr);
+        }
+    }
 
-	public static void i(String content) {
+    public static void i(String content) {
 
-		if (DEFAULT_LEVEL <= LOG_INFO) {
-			LogPrint(LOG_INFO, obtainTag(), content);
-		}
-	}
+        if (DEFAULT_LEVEL <= LOG_INFO) {
+            LogPrint(LOG_INFO, content);
+        }
+    }
 
-	public static void i(String format, Object... args) {
+    public static void i(String format, Object... args) {
 
-		if (DEFAULT_LEVEL <= LOG_INFO) {
-			LogPrint(LOG_INFO, obtainTag(), String.format(format, args));
-		}
-	}
+        if (DEFAULT_LEVEL <= LOG_INFO) {
+            LogPrint(LOG_INFO, String.format(format, args));
+        }
+    }
 
-	public static void i(String content, Throwable tr) {
+    public static void i(String content, Throwable tr) {
 
-		if (DEFAULT_LEVEL <= LOG_INFO) {
-			LogPrint(LOG_INFO, obtainTag(), content, tr);
-		}
-	}
+        if (DEFAULT_LEVEL <= LOG_INFO) {
+            LogPrint(LOG_INFO, content, tr);
+        }
+    }
 
-	public static void w(String content) {
+    public static void w(String content) {
 
-		if (DEFAULT_LEVEL <= LOG_WARN) {
-			LogPrint(LOG_WARN, obtainTag(), content);
-		}
-	}
+        if (DEFAULT_LEVEL <= LOG_WARN) {
+            LogPrint(LOG_WARN, content);
+        }
+    }
 
-	public static void w(String format, Object... args) {
+    public static void w(String format, Object... args) {
 
-		if (DEFAULT_LEVEL <= LOG_WARN) {
-			LogPrint(LOG_WARN, obtainTag(), String.format(format, args));
-		}
-	}
+        if (DEFAULT_LEVEL <= LOG_WARN) {
+            LogPrint(LOG_WARN, String.format(format, args));
+        }
+    }
 
-	public static void w(String content, Throwable tr) {
+    public static void w(String content, Throwable tr) {
 
-		if (DEFAULT_LEVEL <= LOG_WARN) {
-			LogPrint(LOG_WARN, obtainTag(), content, tr);
-		}
-	}
+        if (DEFAULT_LEVEL <= LOG_WARN) {
+            LogPrint(LOG_WARN, content, tr);
+        }
+    }
 
-	public static void e(String content) {
+    public static void e(String content) {
 
-		if (DEFAULT_LEVEL <= LOG_ERROR) {
-			LogPrint(LOG_ERROR, obtainTag(), content);
-		}
-	}
+        if (DEFAULT_LEVEL <= LOG_ERROR) {
+            LogPrint(LOG_ERROR, content);
+        }
+    }
 
-	public static void e(String format, Object... args) {
+    public static void e(String format, Object... args) {
 
-		if (DEFAULT_LEVEL <= LOG_ERROR) {
-			LogPrint(LOG_ERROR, obtainTag(), String.format(format, args));
-		}
-	}
+        if (DEFAULT_LEVEL <= LOG_ERROR) {
+            LogPrint(LOG_ERROR, String.format(format, args));
+        }
+    }
 
-	public static void e(String content, Throwable tr) {
+    public static void e(String content, Throwable tr) {
 
-		if (DEFAULT_LEVEL <= LOG_ERROR) {
-			LogPrint(LOG_ERROR, obtainTag(), content, tr);
-		}
-	}
+        if (DEFAULT_LEVEL <= LOG_ERROR) {
+            LogPrint(LOG_ERROR, content, tr);
+        }
+    }
 
-	public static void wft(String content) {
+    public static void wft(String content) {
 
-		if (DEFAULT_LEVEL <= LOG_ASSERT) {
+        if (DEFAULT_LEVEL <= LOG_ASSERT) {
 
-			LogPrint(LOG_ASSERT, obtainTag(), content);
-		}
-	}
+            LogPrint(LOG_ASSERT, content);
+        }
+    }
 
-	public static void wft(String format, Object... args) {
+    public static void wft(String format, Object... args) {
 
-		if (DEFAULT_LEVEL <= LOG_ASSERT) {
-			LogPrint(LOG_ASSERT, obtainTag(), String.format(format, args));
-		}
-	}
+        if (DEFAULT_LEVEL <= LOG_ASSERT) {
+            LogPrint(LOG_ASSERT, String.format(format, args));
+        }
+    }
 
-	public static void wft(String content, Throwable tr) {
+    public static void wft(String content, Throwable tr) {
 
-		if (DEFAULT_LEVEL <= LOG_ASSERT) {
+        if (DEFAULT_LEVEL <= LOG_ASSERT) {
 
-			LogPrint(LOG_ASSERT, obtainTag(), content, tr);
-		}
-	}
+            LogPrint(LOG_ASSERT, content, tr);
+        }
+    }
 
-	private static String obtainTag() {
+    private static String wrapMessage(String msg) {
+        StackTraceElement caller = Thread.currentThread().getStackTrace()[4];
 
-		if (!TextUtils.isEmpty(DEFAULT_TAG)) {
-			return DEFAULT_TAG;
-		}
+        String callerClazzName = caller.getClassName();
+        callerClazzName = callerClazzName.substring(callerClazzName
+                .lastIndexOf(".") + 1);
 
-		StackTraceElement caller = Thread.currentThread().getStackTrace()[4];
+        return String.format(
+                Locale.ENGLISH,
+                "[%s.%s(L:%d)]----------------------------------------------------------------\n%s",
+                new Object[]{callerClazzName, caller.getMethodName(),
+                        Integer.valueOf(caller.getLineNumber())},msg);
+    }
 
-		String callerClazzName = caller.getClassName();
-		callerClazzName = callerClazzName.substring(callerClazzName
-				.lastIndexOf(".") + 1);
+    private static void LogPrint(int logLevel, String msg) {
 
-		return String.format(
-				Locale.ENGLISH,
-				"%s.%s(L:%d)",
-				new Object[]{callerClazzName, caller.getMethodName(),
-						Integer.valueOf(caller.getLineNumber())});
-	}
+        Log.println(logLevel, DEFAULT_TAG, IS_SHOW_DETAIL ? wrapMessage(msg) : msg);
+    }
 
-	private static void LogPrint(int logLevel, String tag, String msg) {
-
-		Log.println(logLevel, tag, msg);
-	}
-
-	private static void LogPrint(int logLevel, String tag, String msg,
-								 Throwable tr) {
-		Log.println(logLevel, tag, msg + Log.getStackTraceString(tr));
-	}
+    private static void LogPrint(int logLevel, String msg,
+                                 Throwable tr) {
+        Log.println(logLevel, DEFAULT_TAG, IS_SHOW_DETAIL ? wrapMessage(msg + "\n" + Log.getStackTraceString(tr)) : msg + "\n" + Log.getStackTraceString(tr));
+    }
 }
