a503754aef28ff5c54f3e92bf7af6c5c695f56bf
GKerison <gaoxiangyangprivate@gmail.com>
2015-09-08 16:00:45 +0800

    Update UExceptionKit

--- logkit/src/main/java/cn/kerison/kit/log/UExceptionKit.java
+++ logkit/src/main/java/cn/kerison/kit/log/UExceptionKit.java
@@ -1,26 +1,44 @@
 package cn.kerison.kit.log;
 
 import android.content.Context;
-import android.util.StringBuilderPrinter;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.Date;
 
 /**
  * Created by kerison on 2015/9/8.
  */
 public class UExceptionKit implements Thread.UncaughtExceptionHandler {
+    private String mVersionName;
+    private int mVersionCode;
+    private String mPackageName;
 
-
-    private String path;
+    private String mPath;
     private boolean isAppend;
-    private OnAfterExceptionListener listener;
+    private OnAfterExceptionListener mListener;
 
-    public UExceptionKit(Context context, String path, boolean isAppend) {
+    public  static void init(Context context,String path, boolean isAppend,OnAfterExceptionListener listener) {
+        UExceptionKit handler = new UExceptionKit(context,path, isAppend);
+        handler.setOnAfterExceptionListener(listener);
+        Thread.setDefaultUncaughtExceptionHandler(handler);
+    }
 
-        this.path = path;
+    public UExceptionKit(Context context,String path, boolean isAppend) {
+        try {
+            PackageInfo info = context.getPackageManager().getPackageInfo(context.getPackageName(), 1);
+            mVersionName = info.versionName;
+            mVersionCode = info.versionCode;
+            mPackageName = info.packageName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        this.mPath = path;
         this.isAppend = isAppend;
     }
 
@@ -31,36 +49,73 @@ public class UExceptionKit implements Thread.UncaughtExceptionHandler {
         String result = collectInfo();
         LogKit.e(result, ex);
         if (!isAppend) {
-            path = String.format("%s_%tF", path, new Date());
+            mPath = String.format("%s_%tF", mPath, new Date());
         }
-        File file = new File(path);
+        File file = new File(mPath);
         try {
             if (!file.exists()) {
                 file.createNewFile();
             }
             PrintWriter pw = new PrintWriter(new FileOutputStream(file, isAppend));
             pw.println(result);
+            pw.println("------Exption-------");
+            ex.printStackTrace(pw);
+            pw.println("\n\n\n\n");
             pw.flush();
             pw.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        if (this.listener != null) {
-            listener.onAfter();
+        if (this.mListener != null) {
+            mListener.onAfter();
         }
     }
 
     private String collectInfo() {
         StringBuilder result = new StringBuilder();
-        StringBuilderPrinter printer = new StringBuilderPrinter(result);
+        result.append("------ UncaughtException LOG ------");
+        result.append("\n\n");
+
+        Date date = new Date();
+        result.append(String.format("DateTime:%tF%tT", date, date));
+        result.append("\n\n");
+
+        result.append("App packageName: ");
+        result.append(mPackageName);
+        result.append("\n");
+
+        result.append("App versionName: ");
+        result.append(mVersionName);
+        result.append("\n");
+
+        result.append("App versionCode: ");
+        result.append(mVersionCode);
+        result.append("\n");
+
+        result.append("OS Version: ");
+        result.append(Build.VERSION.RELEASE);
+        result.append("_");
+        result.append(Build.VERSION.SDK_INT);
+        result.append("\n\n");
 
 
+        result.append("Vendor: ");
+        result.append(Build.MANUFACTURER);
+        result.append("\n");
+
+        result.append("Model: ");
+        result.append(Build.MODEL);
+        result.append("\n");
+
+        result.append("CPU ABI: ");
+        result.append(Arrays.asList(Build.SUPPORTED_ABIS));
+        result.append("\n\n");
         return result.toString();
     }
 
     public void setOnAfterExceptionListener(OnAfterExceptionListener listener) {
-        this.listener = listener;
+        this.mListener = listener;
     }
 
 
